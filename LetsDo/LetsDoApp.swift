6008c22413d2443b5b9842c3a2c73ece0d89c56d
Viik <frostten06@gmail.com>
2025-01-23 13:27:25 +0900

    Initial Commit

---
+++ LetsDo/LetsDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsDoApp.swift
+//  LetsDo
+//
+//  Created by Hruthviik Raj on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct LetsDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
